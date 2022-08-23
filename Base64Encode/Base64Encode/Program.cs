22917bc32dab891ba94052ece3a363d3180201eb
Claude Agnes <rlawnstjddl12@gmail.com>
2022-08-23 13:57:08 +0900

    Upload Files
    Upload Files

---
+++ Base64Encode/Base64Encode/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Text;
+
+namespace Base64Encode
+{
+    class Program
+    {
+        public static string Base64Encode(string plaintext)
+        {
+            var plainTextBytes = Encoding.UTF8.GetBytes(plaintext);
+            return System.Convert.ToBase64String(plainTextBytes);
+        }
+
+        static void Main(string[] args)
+        {
+            Console.Write("input : ");
+            string text = Console.ReadLine();
+
+            Console.WriteLine("text : " + text);
+            string Encode = Base64Encode(text);
+            Console.WriteLine("Encode : " + Encode);
+        }
+    }
+}
